5c56d24823da6a90ced78925d743b964133836e5
Feyneal <168139030+Feyneal@users.noreply.github.com>
2025-07-02 13:57:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ border_color = "#888" if tema_escuro else "#444"
 
 legend_html = f"""
 <div style='position: fixed;
-     top: 520px; left: 1000px; width: 200px; height: auto;
+     top: 505px; left: 1000px; width: 200px; height: auto;
      z-index:9999; font-size:13px;
      background-color: {bg_color};
      color: {text_color};
