64b0b61b56aba365590bcb2b4ed778f3b3aa115d
Feyneal <168139030+Feyneal@users.noreply.github.com>
2025-07-02 16:52:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ try:
     # Valores absolutos focos de calor
     fc_vals = fc_df[fc_df["Ano"].isin(anos)]["Focos_Anual"].values
 
-    fig, ax = plt.subplots(figsize=(2, 1))
+    fig, ax = plt.subplots(figsize=(4, 2))
 
     # Eixo principal para AQ em hectares
     ax.plot(anos, aq_vals_ha, color="red", marker='o', label="Área Queimada (ha)")
